49b984eeb19ad206a7a785bce10cd2c32891c4b0
hanice <sunhanice@gmail.com>
2015-08-19 03:00:02 -0700

    hanice

---
+++ Data/Ensembl/bowtie2/01-index.sh
@@ -0,0 +1,2 @@
+#bowtie2-build  Homo_sapiens.GRCh38.dna.toplevel.fa  Homo_sapiens.GRCh38
+bowtie2-build-s  Homo_sapiens.GRCh38.dna.toplevel.fa  Homo_sapiens.GRCh38
---
+++ Data/Ensembl/tophat2/01-index-transcriptome.sh
@@ -0,0 +1,1 @@
+tophat -G Homo_sapiens.GRCh38.81.gtf --transcriptome-index=transcriptome-index/Homo_sapiens.GRCh38  /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38
