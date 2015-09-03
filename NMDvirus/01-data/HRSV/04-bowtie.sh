ddadbf41363590b4b1bb84baad080b7347b738a5
hanice <sunhanice@gmail.com>
2015-09-03 03:00:07 -0700

    hanice

---
+++ NMDvirus/01-data/HRSV/04-bowtie.sh
@@ -0,0 +1,24 @@
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399106_1.fastq.gz -2 SRR399106_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399106
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399079_1.fastq.gz -2 SRR399079_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399079
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399319_1.fastq.gz -2 SRR399319_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399319
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399323_1.fastq.gz -2 SRR399323_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399323
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -U SRR400010.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR400010
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -U SRR399324.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399324
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399315_1.fastq.gz -2 SRR399315_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399315
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -U SRR400018.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR400018
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR400017_1.fastq.gz -2 SRR400017_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR400017
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399096_1.fastq.gz -2 SRR399096_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399096
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR400016_1.fastq.gz -2 SRR400016_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR400016
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399105_1.fastq.gz -2 SRR399105_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399105
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399322_1.fastq.gz -2 SRR399322_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399322
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -U SRR399035.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399035
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR400011_1.fastq.gz -2 SRR400011_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR400011
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -U SRR399317.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399317
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399316_1.fastq.gz -2 SRR399316_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399316
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -U SRR396795.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR396795
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR400009_1.fastq.gz -2 SRR400009_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR400009
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR396778_1.fastq.gz -2 SRR396778_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR396778
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -U SRR399321.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399321
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399320_1.fastq.gz -2 SRR399320_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399320
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -1 SRR399099_1.fastq.gz -2 SRR399099_2.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399099
+bowtie2 -p 8 -x /mnt/larsix/projects/NMD/hansun/Data/Ensembl/bowtie2/Homo_sapiens.GRCh38 -U SRR399107.fastq.gz | samtools view -q 30 -bS - | samtools sort - SRR399107
