e82b7fe54542a6284a6b0f365767be3390df97e7
hanice <sunhanice@gmail.com>
2015-09-11 03:00:02 -0700

    hanice

---
+++ NMDvirus/02-count/RSV.2/deNS1/01-count.sh
@@ -0,0 +1,4 @@
+htseq-count -f bam -s no delNS1_M0.1-rep1.bam Homo_sapiens.GRCh38.81.gtf > delNS1_M0.1-rep1.count
+htseq-count -f bam -s no delNS1_M3-rep1.bam Homo_sapiens.GRCh38.81.gtf > delNS1_M3-rep1.count
+htseq-count -f bam -s no delNS1_M3-rep2.bam Homo_sapiens.GRCh38.81.gtf > delNS1_M3-rep2.count
+htseq-count -f bam -s no delNS1_M0.1-rep2.bam Homo_sapiens.GRCh38.81.gtf > delNS1_M0.1-rep2.count
