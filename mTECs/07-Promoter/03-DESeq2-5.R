4201f620fad2060c1e886bf4df2fcb31ce9db754
hanice <sunhanice@gmail.com>
2015-10-26 03:00:02 -0700

    hanice

--- mTECs/07-Promoter/03-DESeq2-5.R
+++ mTECs/07-Promoter/03-DESeq2-5.R
@@ -3,7 +3,7 @@ library(DESeq2)
 geneCounts = read.table('Mouse_Gene_Promoter_Cov', header=T)
 rownames(geneCounts) = paste(geneCounts$gene,geneCounts$chr,geneCounts$start,geneCounts$end,sep='_')
 sampleAnnot = read.table('SampleAnnot',head=T)
-wh = c(1,2,5,6)
+wh = c(1,2,3,4,5,6,7,8)
 sampleAnnot = droplevels(sampleAnnot[wh, ])
 sampleAnnot$ConditionMHCII = relevel(sampleAnnot$ConditionMHCII, 'low') 
 rownames(sampleAnnot) = sampleAnnot$Sample
@@ -24,28 +24,28 @@ res.sig.down = res.sig[res.sig$log2FoldChange<0,]
 rld = rlog(dds, blind=FALSE)
 
 #..#write.table( res, file=file.path(outfolder, "deCP1vCP4.txt"), quote = FALSE, sep = "\t",  row.names = FALSE)
-write.table( res, file="Tspan8_positive_MHCII_HighLow.txt", quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
-write.table( res.sig, file="Tspan8_positive_MHCII_HighLow_sig.txt", quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
-write.table( res.sig.up, file="Tspan8_positive_MHCII_HighLow_sig_up.txt", quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
-write.table( res.sig.down, file="Tspan8_positive_MHCII_HighLow_sig_down.txt", quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
+write.table( res, file="Tspan8_PositiveNegative_MHCII_HighLow.txt", quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
+write.table( res.sig, file="Tspan8_PositiveNegative_MHCII_HighLow_sig.txt", quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
+write.table( res.sig.up, file="Tspan8_PositiveNegative_MHCII_HighLow_sig_up.txt", quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
+write.table( res.sig.down, file="Tspan8_PositiveNegative_MHCII_HighLow_sig_down.txt", quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
 
-save(dds, rld, res, res.sig,sampleAnnot,file="res_Tspan8_positive_MHCII_HighLow.rda")
+save(dds, rld, res, res.sig,sampleAnnot,file="res_Tspan8_PositiveNegative_MHCII_HighLow.rda")
 
-pdf("plot_PCA-Tspan8_positive_MHCII_HighLow.pdf", width=8, height=6)
+pdf("plot_PCA-Tspan8_PositiveNegative_MHCII_HighLow.pdf", width=8, height=6)
 print(plotPCA(rld, intgroup=c("Sample", "ConditionMHCII")))
 dev.off()
 
-pdf("plot_MA-Tspan8_positive_MHCII_HighLow.pdf", width=8, height=6)
+pdf("plot_MA-Tspan8_PositiveNegative_MHCII_HighLow.pdf", width=8, height=6)
 plotMA(results(dds), alpha=0.01)
 dev.off()
 
-pdf("plot_dispEst-Tspan8_positive_MHCII_HighLow.pdf", width=8, height=6)
+pdf("plot_dispEst-Tspan8_PositiveNegative_MHCII_HighLow.pdf", width=8, height=6)
 plotDispEsts(dds)
 dev.off()
 
 
 geneCounts.norm = counts(dds, normalized=T)
-write.table(geneCounts.norm, file="Tspan8_positive_MHCII_HighLow-Counts-Normalized.txt",quote=F,sep="\t",row.names=T,col.names=NA)
+write.table(geneCounts.norm, file="Tspan8_PositiveNegative_MHCII_HighLow-Counts-Normalized.txt",quote=F,sep="\t",row.names=T,col.names=NA)
 
 geneCounts.norm = counts(dds, normalized=F)
-write.table(geneCounts.norm, file="Tspan8_positive_MHCII_HighLow-Counts-raw.txt",quote=F,sep="\t",row.names=T,col.names=NA)
+write.table(geneCounts.norm, file="Tspan8_PositiveNegative_MHCII_HighLow-Counts-raw.txt",quote=F,sep="\t",row.names=T,col.names=NA)
