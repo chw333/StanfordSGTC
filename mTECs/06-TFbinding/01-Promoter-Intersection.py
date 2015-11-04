0572267818c60e4442716ad9d16fa2ff4546728f
hanice <sunhanice@gmail.com>
2015-11-04 03:00:02 -0800

    hanice

--- mTECs/06-TFbinding/01-Promoter-Intersection.py
+++ mTECs/06-TFbinding/01-Promoter-Intersection.py
@@ -8,14 +8,29 @@ inFile.close()
 
 def intersect(inF):
     inFile = open(inF)
+    ouFile = open(inF.split('.txt')[0] + '.Feature', 'w')
     head = inFile.readline()
     for line in inFile:
+        S = set()
         line = line.strip()
         fields = line.split('\t')
         region = fields[0].split('_')
-        ch = 'chr' + region[0]
-        start = int(region[1])
-        end = int(region[2])
+        ch = 'chr' + region[1]
+        start = int(region[2])
+        end = int(region[3])
+        for item in L:
+            if item[0] == ch:
+                if int(item[4]) < start or int(item[3]) > end:
+                    pass
+                else:
+                    S.add('|'.join(item))
+        ouFile.write(line + '\t' + '\t'.join(S) + '\n')
+
+
     inFile.close()
+    ouFile.close()
 
 intersect('MHCII_high_Tspan8_PositiveNegative_sig.txt')
+intersect('Tspan8_positive_MHCII_HighLow_sig.txt')
+intersect('MHCII_low_Tspan8_PositiveNegative_sig.txt')
+intersect('Tspan8_negative_MHCII_HighLow_sig.txt')
---
+++ mTECs/08-InsertSize/02-closest-gene.py
@@ -0,0 +1,57 @@
+def gene_annot():
+    D = {}
+    inFile = open('Mouse_Gene_Annotation_TSS.txt')
+    head = inFile.readline()
+    for line in inFile:
+        line = line.strip()
+        fields = line.split('\t')
+        geneID = fields[0]
+        geneSym = fields[1]
+        ch = fields[2]
+        strand = int(fields[5])
+        start = int(fields[3])
+        end = int(fields[4])
+        TSS = fields[-1]
+        D.setdefault(ch, [])
+        D[ch].append('\t'.join(fields[0:2]+[TSS]+fields[2:]))
+    inFile.close()
+    return D
+
+def closest_gene(inF):
+    D = gene_annot()
+    inFile = open(inF)
+    for line in inFile:
+        line = line.strip()
+        fields = line.split('\t')
+        ch = fields[1]
+        pos = fields[2]
+        D[ch].append(line)
+    inFile.close()
+    for k in D:
+        D[k].sort(cmp = lambda x,y:cmp(int(x.split('\t')[2]), int(y.split('\t')[2])))
+    
+    inFile = open(inF)
+    ouFile = open(inF + '.ClosestGene', 'w')
+    for line in inFile:
+        line = line.strip()
+        ch = line.split('\t')[1]
+        ind = D[ch].index(line)
+        for i in range(ind-1,0,-1):
+            if D[ch][i].find('ENSMUSG') == 0:
+                left = int(line.split('\t')[2]) - int(D[ch][i].split('\t')[2])
+                iL = i
+                break
+        for i in range(ind+1,len(D[ch])):
+            if D[ch][i].find('ENSMUSG') == 0: 
+                right = int(D[ch][i].split('\t')[2]) - int(line.split('\t')[2]) 
+                iR = i
+                break
+        if left < right:
+            ouFile.write(line + '\t' + str(left)+'\t' + D[ch][iL] + '\n')
+        else:
+            ouFile.write(line + '\t' + str(right) + '\t' + D[ch][iR] + '\n')
+
+    inFile.close()
+    ouFile.close()
+
+closest_gene('Tspan8_negative_MHCII_high_rep1.ReadInfo')
