7a11175262317c763e48b976b832a7e29782ed3b
hanice <sunhanice@gmail.com>
2015-12-01 03:00:04 -0800

    hanice

---
+++ mTECs/11-PublicData/01-GSE75262/01-download.sh
@@ -0,0 +1,4 @@
+wget ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR293/SRR2939093/SRR2939093.sra
+wget ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR293/SRR2939094/SRR2939094.sra
+wget ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR293/SRR2939095/SRR2939095.sra
+wget ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR293/SRR2939096/SRR2939096.sra
