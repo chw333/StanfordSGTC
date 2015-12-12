cc373052188f7b95192f5fa034c42c4723c436c4
hanice <sunhanice@gmail.com>
2015-12-12 03:00:03 -0800

    hanice

--- mTECs/11-PublicData/07-GSE63137/05-remove-duplicates.sh
+++ mTECs/11-PublicData/07-GSE63137/05-remove-duplicates.sh
@@ -12,14 +12,14 @@ picard=/mnt/larsix/projects/NMD/hansun/MySoft/picard/picard-tools-1.139
 #java -Xmx4g -jar $picard/picard.jar MarkDuplicates INPUT=${bam}.bam OUTPUT=${bam}_RD.bam REMOVE_DUPLICATES=true  METRICS_FILE=${bam}.metrics.txt
 
 
-bam=PV_Neurons_rep2
+#bam=PV_Neurons_rep2
+#java -Xmx4g -jar $picard/picard.jar MarkDuplicates INPUT=${bam}.bam OUTPUT=${bam}_RD.bam REMOVE_DUPLICATES=true  METRICS_FILE=${bam}.metrics.txt
+
+bam=VIP_Neurons_rep1
 java -Xmx4g -jar $picard/picard.jar MarkDuplicates INPUT=${bam}.bam OUTPUT=${bam}_RD.bam REMOVE_DUPLICATES=true  METRICS_FILE=${bam}.metrics.txt
 
-#bam=VIP_Neurons_rep1
-#java -Xmx4g -jar $picard/picard.jar MarkDuplicates INPUT=${bam}.bam OUTPUT=${bam}_RD.bam REMOVE_DUPLICATES=true  METRICS_FILE=${bam}.metrics.txt
-
-#bam=VIP_Neurons_rep2
-#java -Xmx4g -jar $picard/picard.jar MarkDuplicates INPUT=${bam}.bam OUTPUT=${bam}_RD.bam REMOVE_DUPLICATES=true  METRICS_FILE=${bam}.metrics.txt
+bam=VIP_Neurons_rep2
+java -Xmx4g -jar $picard/picard.jar MarkDuplicates INPUT=${bam}.bam OUTPUT=${bam}_RD.bam REMOVE_DUPLICATES=true  METRICS_FILE=${bam}.metrics.txt
 
 
 
