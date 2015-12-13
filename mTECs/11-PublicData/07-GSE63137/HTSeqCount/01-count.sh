724b19f96321506235c86c7b0a46aafbe193767a
hanice <sunhanice@gmail.com>
2015-12-13 03:00:02 -0800

    hanice

--- mTECs/11-PublicData/07-GSE63137/HTSeqCount/01-count.sh
+++ mTECs/11-PublicData/07-GSE63137/HTSeqCount/01-count.sh
@@ -1,4 +1,6 @@
-htseq-count -f bam -s no PV_Neurons_rep2_NF.bam Mouse_Gene_Promoter.gtf > PV_Neurons_rep2_NF.count
-htseq-count -f bam -s no Excitatory_Neurons_rep1_NF.bam Mouse_Gene_Promoter.gtf > Excitatory_Neurons_rep1_NF.count
-htseq-count -f bam -s no Excitatory_Neurons_rep2_NF.bam Mouse_Gene_Promoter.gtf > Excitatory_Neurons_rep2_NF.count
-htseq-count -f bam -s no PV_Neurons_rep1_NF.bam Mouse_Gene_Promoter.gtf > PV_Neurons_rep1_NF.count
+#htseq-count -f bam -s no PV_Neurons_rep2_NF.bam Mouse_Gene_Promoter.gtf > PV_Neurons_rep2_NF.count
+#htseq-count -f bam -s no Excitatory_Neurons_rep1_NF.bam Mouse_Gene_Promoter.gtf > Excitatory_Neurons_rep1_NF.count
+#htseq-count -f bam -s no Excitatory_Neurons_rep2_NF.bam Mouse_Gene_Promoter.gtf > Excitatory_Neurons_rep2_NF.count
+#htseq-count -f bam -s no PV_Neurons_rep1_NF.bam Mouse_Gene_Promoter.gtf > PV_Neurons_rep1_NF.count
+htseq-count -f bam -s no VIP_Neurons_rep1_NF.bam Mouse_Gene_Promoter.gtf > VIP_Neurons_rep1_NF.count
+htseq-count -f bam -s no VIP_Neurons_rep2_NF.bam  Mouse_Gene_Promoter.gtf > VIP_Neurons_rep2_NF.count
