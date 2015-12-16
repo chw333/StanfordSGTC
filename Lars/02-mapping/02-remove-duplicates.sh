160343d12f8d8a8a765db32993c8258c1b420968
hanice <sunhanice@gmail.com>
2015-12-16 03:00:03 -0800

    hanice

--- Lars/02-mapping/02-remove-duplicates.sh
+++ Lars/02-mapping/02-remove-duplicates.sh
@@ -1,6 +1,9 @@
 picard=/mnt/larsix/projects/NMD/hansun/MySoft/picard/picard-tools-1.139
-bam=Lars
-java -Xmx4g -jar $picard/picard.jar MarkDuplicates INPUT=${bam}.bam OUTPUT=${bam}_RD.bam REMOVE_DUPLICATES=true  METRICS_FILE=${bam}.metrics.txt
+sample=Lars
+INPUT_BAM=${sample}.bam
+RD_BAM=${sample}_RD.bam
+java -Xmx4g -jar $picard/picard.jar MarkDuplicates INPUT=$INPUT_BAM OUTPUT=$RD_BAM REMOVE_DUPLICATES=true  METRICS_FILE=${sample}.metrics.txt
+samtools index $RD_BAM
 
 
 
--- NGLY1/K562/02-bam/04-highQuality.sh
+++
@@ -1,1 +0,0 @@
-samtools view -b -q 60 K562_WT.bam -o K562_WT_HQ.bam
