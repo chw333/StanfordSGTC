61baff8df5d999cfb809e587ee785b8e44d87aa7
hanice <sunhanice@gmail.com>
2016-01-07 03:00:02 -0800

    hanice

---
+++ Data/Ensembl/variation/01-download.sh
@@ -0,0 +1,5 @@
+wget ftp://ftp.ensembl.org/pub/release-83/variation/vcf/homo_sapiens/Homo_sapiens.vcf.gz
+
+wget ftp://ftp.ensembl.org/pub/release-83/variation/vcf/homo_sapiens/Homo_sapiens_clinically_associated.vcf.gz
+
+wget ftp://ftp.ensembl.org/pub/release-83/variation/vcf/homo_sapiens/Homo_sapiens_phenotype_associated.vcf.gz
