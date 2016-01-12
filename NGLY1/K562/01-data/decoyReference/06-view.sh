8ded1771d553923c6af59ca5c246fb271492b274
hanice <sunhanice@gmail.com>
2016-01-12 03:00:02 -0800

    hanice

---
+++ NGLY1/K562/01-data/decoyReference/06-view.sh
@@ -0,0 +1,26 @@
+#samtools view K562_WT.bam DEL1_chr3_25805712_25805739
+#samtools view K562_WT.bam DEL2_chr3_25805715_25805740
+#samtools view K562_WT.bam INS1_chr3_25805725_25805725
+#samtools view K562_WT.bam DEL3_chr3_25824781_25824822
+#samtools view K562_WT.bam INS2_chr3_25824795_25824795
+
+
+#samtools view K562_K8.bam DEL1_chr3_25805712_25805739
+#samtools view K562_K8.bam DEL2_chr3_25805715_25805740
+#samtools view K562_K8.bam INS1_chr3_25805725_25805725
+#samtools view K562_K8.bam DEL3_chr3_25824781_25824822
+#samtools view K562_K8.bam INS2_chr3_25824795_25824795
+#
+#
+#samtools view K562_K15.bam DEL1_chr3_25805712_25805739
+#samtools view K562_K15.bam DEL2_chr3_25805715_25805740
+#samtools view K562_K15.bam INS1_chr3_25805725_25805725
+#samtools view K562_K15.bam DEL3_chr3_25824781_25824822
+#samtools view K562_K15.bam INS2_chr3_25824795_25824795
+#
+#
+samtools view K562_K20.bam DEL1_chr3_25805712_25805739
+samtools view K562_K20.bam DEL2_chr3_25805715_25805740
+samtools view K562_K20.bam INS1_chr3_25805725_25805725
+samtools view K562_K20.bam DEL3_chr3_25824781_25824822
+samtools view K562_K20.bam INS2_chr3_25824795_25824795
