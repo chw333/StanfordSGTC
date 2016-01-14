4645b764fda56e5d25c89c816babbf47a6d40a7b
hanice <sunhanice@gmail.com>
2016-01-14 03:00:04 -0800

    hanice

---
+++ Projects/FanZhou/IonXpress_072_R_2015_08_13_00_20_21_user_HUA-19-20150812-HX_Auto_user_HUA-19-20150812-HX_56.sh
@@ -0,0 +1,1 @@
+samtools mpileup -uf /mnt/larsix/projects/NMD/hansun/Data/UCSC/ucsc-chr/hg19.fa IonXpress_072_R_2015_08_13_00_20_21_user_HUA-19-20150812-HX_Auto_user_HUA-19-20150812-HX_56.bam | bcftools call -cv - > IonXpress_072_R_2015_08_13_00_20_21_user_HUA-19-20150812-HX_Auto_user_HUA-19-20150812-HX_56.raw.vcf 
---
+++ Projects/FanZhou/IonXpress_085_R_2015_08_13_00_20_21_user_HUA-19-20150812-HX_Auto_user_HUA-19-20150812-HX_56.sh
@@ -0,0 +1,1 @@
+samtools mpileup -uf /mnt/larsix/projects/NMD/hansun/Data/UCSC/ucsc-chr/hg19.fa IonXpress_085_R_2015_08_13_00_20_21_user_HUA-19-20150812-HX_Auto_user_HUA-19-20150812-HX_56.bam | bcftools call -cv - > IonXpress_085_R_2015_08_13_00_20_21_user_HUA-19-20150812-HX_Auto_user_HUA-19-20150812-HX_56.raw.vcf 
