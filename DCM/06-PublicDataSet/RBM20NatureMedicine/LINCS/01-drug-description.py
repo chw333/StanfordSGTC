0ab5d91fa0f24a19de01b84c3cacc9e20a903797
hanice <sunhanice@gmail.com>
2016-03-03 03:00:04 -0800

    hanice

---
+++ DCM/06-PublicDataSet/RBM20NatureMedicine/LINCS/01-drug-description.py
@@ -0,0 +1,21 @@
+import urllib2
+import json
+
+def query(inF):
+    inFile = open(inF)
+    ouFile = open(inF + '_description', 'w')
+    ouFile.write('\t'.join(['name','id','collection','vendor','pubchem_cid','url']) + '\n')
+    for line in inFile:
+        line = line.strip()
+        f = urllib2.urlopen('http://api.lincscloud.org/a2/pertinfo?q={"pert_iname":"%s"}&user_key=lincsdemo'%line)
+        s = f.read()
+        sj = json.loads(s)
+        if 'pert_url' in sj[0]:
+            pert_url=sj[0]['pert_url']
+        else:
+            pert_url=''
+        ouFile.write(sj[0]['pert_iname'] +'\t' + sj[0]['pert_id'] + '\t' + sj[0]['pert_collection'] +'\t'+ sj[0]['pert_vendor'] + '\t'+sj[0]['pubchem_cid'] +'\t' + pert_url+ '\n')
+    inFile.close()
+    ouFile.close()
+
+query('result_UPTAG_summly_n7503_drug_anti-connection_name')
